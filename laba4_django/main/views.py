fbc3b679c147fc4b68ca89db5871e80cbb239c58
daria-mironcuk <dariamironcuk00@gmail.com>
2025-06-26 01:04:18 +0300

    Add files via upload

---
+++ laba4_django/main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def page1(request):
+    return render(request, 'main/page1.html')
+
+def page2(request):
+    return render(request, 'main/page2.html')
+
+def page3(request):
+    return render(request, 'main/page3.html')
+
+def page4(request):
+    return render(request, 'main/page4.html')
+
+def page5(request):
+    return render(request, 'main/page5.html')
